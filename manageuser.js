788b52abadb1d5be90b6fa0571b24504c305279c
jishrami <jramirez@byui.edu>
2023-01-26 10:06:18 -0700

    changed two factor to send to user-input

--- manageuser.js
+++ manageuser.js
@@ -11,10 +11,6 @@ function setusername(){
 
 function setuserpassword(){
     password = $("#password").val();
-    var valid=passwordRegEx.exec(password);
-    if (!valid){
-        alert('Must be 6 digits, upper, lower, number, and symbol');
-    }
 }
 
 function setverifypassword(){
@@ -47,10 +43,10 @@ function checkexpiredtoken(token){
 }
 
 function oneTimePassword(){
-    
+    const phoneNumber = document.getElementById('username').value
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/twofactorlogin/5202576202',
+        url: 'https://dev.stedi.me/twofactorlogin/' + phoneNumber,
         data: JSON.stringify({userName, password}),
         contentType: "application/text",
         dataType: 'text'
@@ -63,8 +59,8 @@ function userlogin(){
     setusername();
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/login',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin',
+        data: JSON.stringify({phoneNumber:'5202576202', oneTimePassword:'5847'}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
