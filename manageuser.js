c21817ecccd46961412027be097b46ac14950a3c
jishrami <jramirez@byui.edu>
2023-01-31 13:16:50 -0700

    changed userName and password in manageuser

--- manageuser.js
+++ manageuser.js
@@ -60,7 +60,7 @@ function userlogin(){
     $.ajax({
         type: 'POST',
         url: 'https://dev.stedi.me/twofactorlogin',
-        data: JSON.stringify({phoneNumber:'5202576202', oneTimePassword:'5847'}),
+        data: JSON.stringify({phoneNumber: userName, oneTimePassword: password}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
